d84090bd0da3ebfe9511cdcc50392e21798da3c9
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2021-10-13 23:48:43 +0300

    1 task1

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const itemElem = document.querySelector('#ingredients')
+
+const listElem = ingredients.forEach((ingredient) => {
+  const itemListElem = document.createElement('li')
+  itemListElem.classList.add('item')
+  itemListElem.textContent = ingredient
+  itemElem.appendChild(itemListElem)
+})
+
+console.log(itemElem)
