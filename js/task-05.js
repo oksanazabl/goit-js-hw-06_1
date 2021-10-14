e1e51bc5793c64e69055899128e225dd56fa637e
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2021-10-15 00:26:36 +0300

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+let inputElem = document.querySelector('#name-input');
+inputElem.addEventListener('input', onInputChange);
+
+let outputElem = document.querySelector('#name-output');
+
+function onInputChange(event) {
+  inputElem = event.currentTarget.value;
+  console.log(inputElem);
+
+  inputElem !== ' ' ? (outputElem.innerText = inputElem.trim()) : 'Anonymous';
+}
