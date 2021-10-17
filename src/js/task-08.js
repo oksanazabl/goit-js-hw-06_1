0ed750ed8621d2f9bec0bdb618b668623b35b3ee
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2021-10-17 22:27:51 +0300

    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,2 +1,11 @@
 'use strict';
+const userForm = document.querySelector('form.login-form');
+console.log(userForm);
+
+function FormSubmit(event) {
+    const userFormElem = currentTarget.elements;
+    const email = userFormElem.email.value;
+    const password = userFormElem.password.value;
+    event.preventDefault();
+}
 
