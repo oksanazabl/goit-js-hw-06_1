575ecbeb485d238b2d37cd475ad830f03ad76ebe
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2021-10-19 13:23:01 +0300

    update task 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,11 +3,11 @@
 const textElem = document.getElementById("text");
 const inputElem = document.getElementById("font-size-control");
 
-textElem.style.fontSize = '50px';
+textElem.style.fontSize = `${inputElem.value}px`;
 
 inputElem.addEventListener("input", (elem) => {
-const size = inputElem.value;
+//const size = inputElem.value;
  
- textElem.style.fontSize = `${size}px`;
+ textElem.style.fontSize = `${inputElem.value}px`;
 });
 
