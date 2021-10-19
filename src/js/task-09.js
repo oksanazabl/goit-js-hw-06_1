38daa2a23c68ba2909309f3b3f836f027dddd489
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2021-10-20 01:24:06 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -4,10 +4,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const body = document.querySelector('body');
+const numColor = document.querySelector('.color');
+
 const setBg = () => {
   const color = getRandomHexColor();
-  document.body.style.backgroundColor = color;
-  color. = getRandomHexColor();
+  body.style.backgroundColor = color;
+  numColor.textContent = color;
 }
 
 genNewCol.addEventListener("click", setBg);
